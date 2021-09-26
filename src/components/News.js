7ffa91410ba04f99e73769869083a875c966bc6d
Aman <aman.sharma11399@gmail.com>
2021-09-26 13:50:28 +0530

    initial commit

---
+++ src/components/News.js
@@ -0,0 +1,110 @@
+import React, { useEffect, useState } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+
+const News = (props) => {
+
+    const [articles, setArticles] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [page, setPage] = useState(1)
+    const [totalResults, setTotalResults] = useState(0)
+
+    const capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const updateNews = async() => {
+        props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pagesize=${props.pageSize}`;
+        setLoading(true)
+
+        let data = await fetch(url);
+
+        
+        props.setProgress(30);
+
+        let parsedData = await data.json()
+
+        props.setProgress(50);
+
+        setArticles(parsedData.articles)
+        setTotalResults(parsedData.totalResults)
+        setLoading(false)
+
+        props.setProgress(100);
+    }
+
+    useEffect(() => {
+        updateNews();
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsGorilla`;
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    // const handlePrevClick = async () => {
+    //     setPage(page-1)
+    //     updateNews();
+    // }
+
+    // const handleNextClick = async () => {
+    //     setPage(page+1)
+    //     updateNews();
+    // }
+    const fetchMoreData = async () => {
+
+        
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pagesize=${props.pageSize}`;
+        setPage(page+1)
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        console.log(parsedData);
+        setArticles(articles.concat(parsedData.articles))
+        setTotalResults(parsedData.totalResults)
+    };
+
+    return (
+        <>
+        <h1 className="text-center">NewsGorilla - Top Headlines from {capitalizeFirstLetter(props.category)}</h1>
+        {loading && <Spinner />}
+        <InfiniteScroll
+            dataLength={articles.length}
+            next={fetchMoreData}
+            hasMore={articles.length !== totalResults}
+            loader={<h4><Spinner /></h4>}
+        >
+        <div className="container">
+
+        
+        <div className="row" >
+            {articles.map((element) => {
+                return <div className="col-md-4" key={element.url} >
+                    <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage}
+                        newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                </div>
+            })}
+        </div>
+        </div>
+
+        </InfiniteScroll>
+
+        </>
+    )
+}
+
+
+News.defaultProps = {
+    country: 'in',
+    pagesize: 8,
+    category: 'general',
+}
+
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+}
+
+export default News
